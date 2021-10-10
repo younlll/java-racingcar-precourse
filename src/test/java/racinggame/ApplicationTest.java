9977e99af9f1097f46abe69323460495cbed972c
이유나 <dragonyuna@naver.com>
2021-10-11 04:36:20 +0900

    예외처리 테스트

--- src/test/java/racinggame/ApplicationTest.java
+++ src/test/java/racinggame/ApplicationTest.java
@@ -25,9 +25,65 @@ public class ApplicationTest extends NSTest {
     }
 
     @Test
+    void 여러명의_우승자() {
+        assertRandomTest(() -> {
+            run("pobi,woni", "1");
+            verify("pobi : -", "woni : -", "최종 우승자는 pobi,woni 입니다.");
+        }, MOVING_FORWARD, MOVING_FORWARD);
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(() -> {
             runNoLineFound("pobi,javaji");
+            verify(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 이름의_입력값이_없는_경우_예외_처리() {
+        assertSimpleTest(() -> {
+            runNoLineFound("\n");
+            verify(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 자동차의_입력이_2대_미만인_경우_예외_처리() {
+        assertSimpleTest(() -> {
+            runNoLineFound("pobi");
+            verify(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 시도횟수가_입력되지_않은_경우_예외_처리() {
+        assertSimpleTest(() -> {
+            runNoLineFound("pobi,woni", "\n");
+            verify(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 시도횟수가_양수가_아닌_경우_예외_처리() {
+        assertSimpleTest(() -> {
+            runNoLineFound("pobi,woni", "-1");
+            verify(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 시도횟수가_범위를_초과하는_경우_예외_처리() {
+        assertSimpleTest(() -> {
+            runNoLineFound("pobi,woni", "12345678999");
+            verify(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 시도횟수_입력에_대한_예외_처리() {
+        assertSimpleTest(() -> {
+            runNoLineFound("pobi,woni", "abc");
             verify(ERROR_MESSAGE);
         });
     }
